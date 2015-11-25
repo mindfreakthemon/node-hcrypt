7416656d1ee4c0fc3f00e180d7514b5edea19eab
Vitalii Rakovtsii <vitalii.rakovtsii@playtech.com>
2015-11-25 11:58:50 +0200

    Moving towards creating a lib

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 node-hcrypt
 ===========
+
+WIP:
+
+```
+sudo apt-get install libgmp-dev libmpfr-dev libmpfr4 libmpfr-dev libgmp10 libgmpxx4ldbl libgmp-dev gcc g++ automake autoconf
++ flint 2.4.5 from the official site
+```
--- index.js
+++ index.js
@@ -1,120 +1,37 @@
 var assert = require('assert'),
-	scarab = require('./build/Release/addon');
+	scarab = require('./lib/scarab');
 
-var crypto,
-	operator;
+var crypto = new scarab.Crypto(),
+	operator = new scarab.Operator(crypto.export());
 
-crypto = new scarab.Crypto();
-operator = new scarab.Operator(crypto.export());
+var t = process.hrtime();
 
-function to_bin_array(x) {
-	return x
-		.toString(2)
-		.split('')
-		.map(Number)
-		.reverse();
-}
+/* CLIENT HAS THE DATA */
 
-function from_bin_array(x) {
-	return parseInt(x
-		.reverse()
-		.map(String)
-		.join(''), 2);
-}
+var a = 22222,
+	b = 321;
 
-function to_crypto_array(x) {
-	return x.map(crypto.encrypt.bind(crypto));
-}
+/* CLIENT ENCODES HIS DATA */
 
-function from_crypto_array(x) {
-	return x.map(crypto.decrypt.bind(crypto));
-}
+var _encoded = scarab.encrypt(crypto, a, b);
 
-function some_add(a, b) {
-	/// PUBLIC DATA
-//	console.log(a);
-//	console.log(b);
-	///
+/* CLIENT SENDS crypto.export() and _encoded TO THE SERVER */
 
-	var _c = null,
-		l = a.length,
-		r = new Array(l),
-		i, v;
+console.log(_encoded);
 
-	for (i = 0; i < l; i++) {
-		if (_c) {
-			v = operator.fullAdd(_c, a[i], b[i]);
-		} else {
-			v = operator.halfAdd(a[i], b[i]);
-		}
+/* SERVER PERFORMS THE OPERATION WITH THE _encoded AND crypto.export() */
 
-		r[i] = v[0];
-		_c = v[1];
-	}
+var _encoded_result = scarab.add(operator, _encoded.a, _encoded.b);
 
-	/// PUBLIC DATA
-//	console.log(r);
-	///
+/* SERVER SENDS RESULT TO THE CLIENT */
 
-	return r;
-}
+/* CLIENT DECRYPTS THE RESULT */
 
-function add(a, b) {
-	/// INPUT
-//	console.log(a, b);
-	///
-
-	var _a = to_bin_array(a),
-		_b = to_bin_array(b),
-		_al = _a.length,
-		_bl = _b.length,
-		_l, _t, _r;
-
-	if (_al !== _bl) {
-		_l = Math.abs(_al - _bl);
-		_t = _al > _bl ? _b : _a;
-
-		while (_l--) {
-			_t.push(0);
-		}
-	}
-
-	assert.equal(_a.length, _b.length);
-
-	_a.push(0);
-	_b.push(0);
-
-	/// PRIVATE DATA
-//	console.log(_a);
-//	console.log(_b);
-
-	_a = to_crypto_array(_a);
-	_b = to_crypto_array(_b);
-
-	_r = some_add(_a, _b);
-	_r = from_crypto_array(_r);
-
-	/// PRIVATE DATA
-//	console.log(_r);
-
-	_r = from_bin_array(_r);
-
-	/// RESULT
-//	console.log(_r);
-	///
-
-	return _r;
-}
-
-var x = 234234234,
-	y = 4234234234,
-	t = process.hrtime();
-
-var r = add(x, y);
+var result = scarab.decrypt(crypto, _encoded_result);
 
 t = process.hrtime(t);
 
-assert.equal(r, x + y);
+assert.equal(result, a + b);
 
 console.log('took %d', t[0] + t[1] * 1e-9);
-console.log('r = %d, should be %d', r, x + y);
+console.log('r = %d, should be %d', result, a + b);
